756a7003c5e412c1611a6d309cc8c9c760138c95
AdrianCM21 <adriancasco2002@gmail.com>
2024-04-24 08:23:02 -0400

    Primer commit

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+
+    public function run()
+    {
+        Role::create(['name'=> 'user']);
+        Role::create(['name'=> 'cliente']);
+    }
+}
